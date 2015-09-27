492c964e3128b10c7aa32769f6b41f5eb2021e1a
v-nayjack <vinay.jakkali@gmail.com>
2015-09-27 16:00:47 -0400

    committing the cachematrix.R to the forked repo

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,21 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function (x = matrix()) {
 
+        I <- NULL
+        set <- function(y = matrix()) {
+                x <<- y
+                I <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) I <<- solve
+        getinv <- function() I
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
+        
+              
 }
 
 
@@ -12,4 +25,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        I <- x$getinv()
+        if(!is.null(I)) {
+                message("getting cached data")
+                return(I)
+        }
+        data <- x$get()
+        I <- solve(data, ...)
+        x$setinv(I)
+        I
+
+                
 }
+
